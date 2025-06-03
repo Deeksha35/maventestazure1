e8c292cb263c0f4fde242be9b226eef5fec363cc
unknown <snaikdeeksha2@gmail.com>
2025-06-03 19:25:34 +0530

    azure pipeline exapmle

---
+++ src/main/java/deenik/deenik/App.java
@@ -0,0 +1,13 @@
+package deenik.deenik;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
